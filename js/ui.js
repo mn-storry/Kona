44442da2da0f123dbc81b894aea8fc9f53259784
LAPTOP-JAJQD1C5\jihyu <storrymn@gmail.com>
2021-05-10 18:00:12 +0900

    파일 수정

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,19 @@
+
 $(function(){
-	
-	$('.filterBox dl dd').hide();
-	$('.filterBox dl dt .btn_filter').on('click', function(){
-		$(this).parent().next().fadeIn();
-	})
+    
+	var win_h = $(window).outerHeight(),
+        header_h = $('#headerWrap').outerHeight(),
+        cont_h = win_h - header_h;
+        
+        cont_pt = $('.content').css('padding-top').replace(/[^-\d\.]/g, ''),
+        cont_pb = $('.content').css('padding-bottom').replace(/[^-\d\.]/g, ''),
+        layout_h = $('.layout').outerHeight();
+        new_h = win_h - header_h - cont_pt - cont_pb;
+        
+        console.log(win_h, header_h, cont_pt, cont_pb);
+        console.log('컨텐츠 :' + cont_h, '레이아웃:'+ layout_h, '적용높이:'  + new_h);
+    
+    if(layout_h < cont_h){
+        $('.layout').outerHeight(new_h);
+    }
 });
